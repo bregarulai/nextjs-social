1e30b43b0da102ee2c42fd5cd9e3faa546f8e482
bregarulai <bregarulai@gmail.com>
2021-12-10 11:19:19 -0500

    added createdAt field to user collection

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -21,25 +21,12 @@ export default NextAuth({
     updateAge: 24 * 60 * 60, // 24 hours
   },
 
-
-  // JSON Web tokens are only used for sessions if the `jwt: true` session
-  // option is set - or by default if no database is specified.
-  // https://next-auth.js.org/configuration/options#jwt
   jwt: {
       secret: process.env.SECRET,
       maxAge: 30 * 24 * 60 * 60, // 30 days
-    // You can define your own encode/decode functions for signing and encryption
-    // if you want to override the default behaviour.
-    // encode: async ({ secret, token, maxAge }) => {},
-    // decode: async ({ secret, token, maxAge }) => {},
   },
   adapter: MongoDBAdapter(clientPromise),
 
-  // You can define custom pages to override the built-in ones. These will be regular Next.js pages
-  // so ensure that they are placed outside of the '/api' folder, e.g. signIn: '/auth/mycustom-signin'
-  // The routes shown here are the default URLs that will be used when a custom
-  // pages is not specified for that route.
-  // https://next-auth.js.org/configuration/pages
   pages: {
     // signIn: '/auth/signin',  // Displays signin buttons
     // signOut: '/auth/signout', // Displays form with sign out button
@@ -48,26 +35,24 @@ export default NextAuth({
     // newUser: null // If set, new users will be directed here on first sign in
   },
 
-  // Callbacks are asynchronous functions you can use to control what happens
-  // when an action is performed.
-  // https://next-auth.js.org/configuration/callbacks
   callbacks: {
-    // async signIn({ user, account, profile, email, credentials }) { return true },
+    async signIn({ user, account, profile, email, credentials }) {
+        user.createdAt = new Date()
+        return user },
     // async redirect({ url, baseUrl }) { return baseUrl },
-    // async session({ session, token, user }) { return session },
-    // async jwt({ token, user, account, profile, isNewUser }) { return token }
+    async session({ session, token}) { 
+        session.user.tag = session.user.name.split(' ').join('').toLocaleLowerCase();
+        session.user.uid = token.sub
+        
+        return session },
+    // async jwt({ token, user, account, profile, isNewUser }) {return token }
   },
 
-  // Events are useful for logging
-  // https://next-auth.js.org/configuration/events
   events: {},
 
-  // You can set the theme to 'light', 'dark' or use 'auto' to default to the
-  // whatever prefers-color-scheme is set to in the browser. Default is 'auto'
   theme: {
     colorScheme: "light",
   },
 
-  // Enable debug messages in the console if you are having problems
   debug: false,
 })
