8c87d6a8f4d6bbd795a46852d21426b623b7d99c
bregarulai <bregarulai@gmail.com>
2021-12-10 18:27:10 -0500

    created feed header and create post component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { SparklesIcon } from '@heroicons/react/outline';
+
+import { CreatePost } from '../components';
 
 const Feed = () => {
   return (
     <div className='xl:ml-[20rem] sm:ml-[5rem] flex-grow max-w-4xl border-l border-r border-gray-700 min-h-screen'>
-      Feed
+      <div className='text-[#d9d9d9] flex items-center sm:justify-between py-4 px-3 sticky top-0 z-50 border-b border-gray-700'>
+        <h2 className='text-lg sm:text-lg font-bold'>Home</h2>
+        <div className='hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0 ml-auto'>
+          <SparklesIcon className='h-5 text-white' />
+        </div>
+      </div>
+      <CreatePost />
     </div>
   );
 };
--- components/index.js
+++ components/index.js
@@ -1,3 +1,4 @@
+export {default as CreatePost} from './CreatePost'
 export {default as Feed} from './Feed'
 export {default as SidebarLink} from './SidebarLink'
 export {default as Sidebar} from './Sidebar'
