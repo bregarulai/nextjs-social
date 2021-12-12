2ef03145b85378e9bec81368cf840e44b80bca87
bregarulai <bregarulai@gmail.com>
2021-12-11 20:48:38 -0500

    user info and post image in post component

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -41,7 +41,7 @@ const Feed = () => {
 
   return (
     <div className='xl:ml-[20rem] sm:ml-[5rem] flex-grow max-w-4xl border-l border-r border-gray-700 min-h-screen'>
-      <div className='text-[#d9d9d9] flex items-center sm:justify-between py-4 px-3 sticky top-0 z-50 border-b border-gray-700'>
+      <div className='text-[#d9d9d9] flex items-center bg-slate-900 sm:justify-between py-4 px-3 sticky top-0 z-50 border-b border-gray-700'>
         <h2 className='text-lg sm:text-lg font-bold'>Home</h2>
         <div className='hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0 ml-auto'>
           <SparklesIcon className='h-5 text-white' />
--- components/Post.jsx
+++ components/Post.jsx
@@ -1,8 +1,82 @@
 import React from 'react';
+import Moment from 'react-moment';
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+  SwitchHorizontalIcon,
+  TrashIcon,
+} from '@heroicons/react/outline';
 
-const Post = ({ post }) => {
+const Post = ({ post, postPage }) => {
   console.log('POST: ', post);
-  return <div>Post component</div>;
+  return (
+    <div className='flex p-3 cursor-pointer border-b border-gray-700'>
+      {!postPage && (
+        <img
+          src={post?.userImg}
+          alt={post?.username}
+          className='w-11 h-11 mr-4 rounded-full'
+        />
+      )}
+      <div className='flex flex-col space-y-2 w-full'>
+        <div className={`flex ${!postPage && 'justify-between'}`}>
+          {postPage && (
+            <img
+              src={post?.userImg}
+              alt='Profile Pic'
+              className='h-11 w-11 rounded-full mr-4'
+            />
+          )}
+          <div className='text-[#6e767d]'>
+            <div className='group inline-block'>
+              <h4
+                className={`font-bold text-[15px] sm:text-base text-[#d9d9d9] group-hover:underline ${
+                  !postPage && 'inline-block'
+                }`}
+              >
+                {post?.username}
+              </h4>
+              <span
+                className={`text-sm sm:text-[15px] ${!postPage && 'ml-1.5'}`}
+              >
+                @{post?.tag}
+              </span>
+            </div>
+            .{' '}
+            <span className='hover:underline text-sm sm:text-[15px]'>
+              <Moment fromNow>{post?.timestamp?.toDate()}</Moment>
+            </span>
+            {!postPage && (
+              <p className='text-[15px] text-[#d9d9d9] sm:text-base mt-0.5'>
+                {post?.text}
+              </p>
+            )}
+          </div>
+          <div className='icon flex-shrink-0 group ml-auto'>
+            <DotsHorizontalIcon className='h-5 text-[#6e767d] group-hover:text-[#1d9bf0]' />
+          </div>
+        </div>
+        {postPage && (
+          <p className='text-[#d9d9d9] mt-0.5 text-xl'>{post?.text}</p>
+        )}
+        <img
+          src={post?.image}
+          alt={post?.text}
+          className='rounded-2xl max-h-[700px] object-cover mr-2'
+        />
+        <div>
+          <div className='group flex items-center space-x-1'>
+            <div>
+              <ChatIcon className='group-hover:text-[#1d9bf0] h-5' />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Post;
