fa562f427f85085dc2737011273e2b970fdcb76e
bregarulai <bregarulai@gmail.com>
2021-12-17 09:03:59 -0500

    fetching post data

--- pages/[id].jsx
+++ pages/[id].jsx
@@ -1,13 +1,22 @@
+import { doc, onSnapshot } from 'firebase/firestore';
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
 import { useRouter } from 'next/router';
 import React, { useState, useEffect } from 'react';
+import { db } from '../firebase';
 
 const PostPage = () => {
   const [comments, setComments] = useState([]);
   const [posts, setPosts] = useState([]);
   const { data: session } = useSession();
   const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() =>
+    onSnapshot(doc(db, 'post', id), (snapshot) => setPosts(snapshot.data()), [
+      db,
+    ])
+  );
   return (
     <div>
       <Head></Head>
