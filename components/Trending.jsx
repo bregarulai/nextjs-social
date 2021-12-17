300c29574ad3cbf86adb413e55baa53f84a3be4b
bregarulai <bregarulai@gmail.com>
2021-12-17 11:17:35 -0500

    display heading on trending component

--- components/Trending.jsx
+++ components/Trending.jsx
@@ -1,7 +1,14 @@
+import { Result } from 'postcss';
 import React from 'react';
 
 const Trending = ({ trending }) => {
-  return <div>Trending</div>;
+  return (
+    <div className='hover:bg-white hover:bg-opacity-[0.03] px-4 py-2 cursor-pointer transition duration-200 ease-out flex items-center justify-between'>
+      <div className='space-y-0.5'>
+        <p className='text-sm text-[#6e767d] font-medium'>{trending.heading}</p>
+      </div>
+    </div>
+  );
 };
 
 export default Trending;
