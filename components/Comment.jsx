299126a4a9524a144af38b5c6ba46ce62749922e
bregarulai <bregarulai@gmail.com>
2021-12-17 10:14:48 -0500

    finished post page

--- components/Comment.jsx
+++ components/Comment.jsx
@@ -1,3 +1,10 @@
+import {
+  ChartBarIcon,
+  ChatIcon,
+  DotsHorizontalIcon,
+  HeartIcon,
+  ShareIcon,
+} from '@heroicons/react/outline';
 import React from 'react';
 import Moment from 'react-moment';
 
@@ -27,6 +34,26 @@ const Comment = ({ comment }) => {
             <p className='mt-0.5 text-[#d9d9d9] max-w-lg overflow-scroll text-[15px] sm:text-base scrollbar-hide'>
               {comment.comment}
             </p>
+          </div>
+          <div className='group icon flex-shrink-0'>
+            <DotsHorizontalIcon className='h-5 text-[#63767d] group-hover:text-indigo-500' />
+          </div>
+        </div>
+        <div className='flex justify-between w-10/12 text-[#6e767d]'>
+          <div className='group icon'>
+            <ChatIcon className='h-5 group-hover:text-[#1d9bf0]' />
+          </div>
+          <div className='flex items-center group space-x-1'>
+            <div className='group icon group-hover:bg-pink-600/10'>
+              <HeartIcon className='h-5 group-hover:text-pink-600' />
+            </div>
+            <span className='group-hover:text-pink-600'></span>
+          </div>
+          <div className='group icon'>
+            <ShareIcon className='h-5 group-hover:text-[#1d9bf0]' />
+          </div>
+          <div className='group icon'>
+            <ChartBarIcon className='h-5 group-hover:text-[#1d9bf0] rotate-90' />
           </div>
         </div>
       </div>
